1767bc421feca11b4157ac429850e552fdd8c703
dsauce5 <dsauce5@illinois.edu>
2018-09-19 18:20:04 -0500

    Starting project commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Here");
+    }
+}
