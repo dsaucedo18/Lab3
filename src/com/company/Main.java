b6cfa96bdfea557ef0b78d49c0a2a335f663656e
dsauce5 <dsauce5@illinois.edu>
2018-09-19 18:48:51 -0500

    project commit 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,29 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Here");
+        // write your code here
+        String website = "http://erdani.com/tdpl/hamlet.txt";
+        String websiteTwo = "https://www.bls.gov/tus/charts/chart9.txt";
+        String websiteThree = "http://tgftp.nws.noaa.gov/data/raw/fz/fzus53.klot.srf.lot.txt";
+        System.out.println(urlToString(website));
+        for (int = 0; )
+    }
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
     }
 }
